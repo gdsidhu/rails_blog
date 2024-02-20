8484ddf02a724fef435b00e5a673c3d31b4bedb6
gdsidhu <102585578+gdsidhu@users.noreply.github.com>
2024-02-20 21:07:17 +0530

    added some style

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,6 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema[7.1].define(version: 2024_02_20_114424) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
